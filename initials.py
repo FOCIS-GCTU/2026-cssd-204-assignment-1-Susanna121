040b3a6f94356dc40c92def3f12bc59ed4c133ac
Susanna121 <2425400613@live.gctu.edu.gh>
2026-06-01 17:26:18 +0100

    Add function to print initials in large letters

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials in large letters.
+# Assignment Number: 1
+# Name: Susanna Amoako
+# STUDENT ID: 2425400613
+# Email: [susannaamoako0@gmail.com](mailto:susannaamoako0@gmail.com)
+# Grader: Augustus Buckman
+
+# On my honor, Susanna Amoako, this programming assignment is my own work and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+# Print my initials in large letters
 
+```
+print()
+print("...SNA")
+print()
 
+print("...SSSSSSSSSSSS........NN........NN........AAAAAAAAAAAA.....")
+print("...SSSSSSSSSSSS........NNN.......NN........AAAAAAAAAAAA.....")
+print("...SSS.................NNNN......NN........AAA......AAA.....")
+print("...SSS.................NN.NN.....NN........AAA......AAA.....")
+print("...SSSSSSSSSSSS........NN..NN....NN........AAAAAAAAAAAA.....")
+print("...SSSSSSSSSSSS........NN...NN...NN........AAAAAAAAAAAA.....")
+print("............SSS........NN....NN..NN........AAA......AAA.....")
+print("............SSS........NN.....NN.NN........AAA......AAA.....")
+print("...SSSSSSSSSSSS...**...NN......NNNN...**...AAA......AAA**...")
+print("...SSSSSSSSSSSS...**...NN.......NNN...**...AAA......AAA**...")
+
+print()
+```
 
 main()
